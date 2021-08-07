847de9b6989ed2f599cf00f1196dbb08a6ddd804
seunjosh <seunaji2015@gmail.com>
2021-08-06 18:56:27 -0700

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          HELLO AJETEC WORLD.   
+        </p>
+        <p>We Handle all IT issues</p>
+
+          <p>We deliver top-notch services globally</p>
+
+          <p>We Move</p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
